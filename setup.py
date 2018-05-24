062d23614f3e60c3edfca412b43ab88741936999
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2018-05-24 16:14:17 +0200

    fixing pytest settings in setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
     def initialize_options(self):
         TestCommand.initialize_options(self)
-        self.pytest_args = ['deeptime.tae']
+        self.pytest_args = ['deeptime/tae']
     def run_tests(self):
         import pytest
         errno = pytest.main(self.pytest_args)
